623c5411b6217a728d1f4b93e7be3584b5620b27
Clifford Heath <clifford.heath@gmail.com>
2025-05-11 11:36:46 +1000

    Added fixup script to make bulk edits on the imported data

---
+++ fixup.rb
@@ -0,0 +1,122 @@
+# ruby -w
+#
+# Fixup script for JLC library imported using easyeda2kicad
+#
+require 'kicad'
+require 'debug'
+
+=begin
+For all symbols
+* create (property "PartNumber"...) to be a copy of (property "Value"...) but (at 0 0 0)
+* change (property "Datasheet" to be (at 0 0 0)
+* add (hide yes) to (effects) of (property "LCSC Part")
++ change (property "Footprint" to be (at 0 0 0)
+
+For each resistor and capacitor:
+* add (pin_numbers (hide yes))
+* add (pin_names (hide yes))
+* change (property "Reference" to be (at 0 2.54 0)
+* change (property "Value" to be (at 0 -2.54 0)
+- change (property "Value" to be the semantic value of the part
+- populate the Description property with text from the appropriate field on the LCSC web page for the part
+=end
+
+k = KiCad.load('jlcbasic.kicad_sym').value
+
+k.all_symbol.
+  each do |s|
+    # Create PartNumber from Value if there isn't one
+    if s.property('PartNumber')
+      s['PartNumber'] = s.property('Value')
+    end
+
+    # Hide "LCSC Part" is present
+    if s.property('LCSC Part')
+      s.property_node('LCSC Part').hidden = true
+    end
+
+    footprint_node = s.property_node('Footprint')
+    if footprint_node
+      at = footprint_node.at
+      if at && (at.x != 0 || at.y != 0 || at.angle != 0)
+        puts "#{s.id} has footprint at (#{at.x}, #{at.y}, #{at.angle}) - not resetting"
+      end
+    end
+  end
+
+# Move all Datasheet properties to be at 0,0,0, and hidden
+k.all_symbol.           # All top-level symbols
+  map do |s|            # The property node for the data sheet property, if any
+    s.property_node("Datasheet")
+  end.
+  each do |ds|          # and it's not positioned at 0,0,0
+    next nil unless ds  # Skip if there's no Datasheet property
+
+    # Hide it
+    ds.hidden = true
+
+    # Move it to 0,0,0:
+    at = ds.at
+    if at && (at.x != 0 || at.y != 0 || at.angle != 0)
+      at.x = at.y = at.angle = 0
+    end
+  end
+
+# Hide a pin_names or pin_numbers node:
+def hide p
+  if p.hide
+    p.hide.hide = :yes
+  else  # Add a hide node
+    p.children.append(KiCad.parse('(hide yes)')&.value)
+  end
+end
+
+# For all R's and C's:
+k.all_symbol.
+  each do |s|
+    reference = s.property('Reference')
+    next unless ['R', 'C'].include?(reference)
+
+    # Hide PartNumbers:
+    s.property_node('PartNumber')&.hidden = true
+
+    # Reposition Reference and Value display:
+    reference_node = s.property_node('Reference')
+    at = reference_node.at
+    if at
+      at.x = at.angle = 0
+      at.y = 2.54
+    else
+      puts "#{s.id} has no Reference property to move"
+    end
+
+    # Move the Value:
+    value_node = s.property_node('Value')
+    at = value_node.at
+    if at
+      at.x = at.angle = 0
+      at.y = -2.54
+    else
+      puts "#{s.id} has no Value property to move"
+    end
+
+    # If there are pin names, hide them:
+    pin_name_node = s.property_node('pin_names')
+    if pin_name_node
+      hide(pin_name_node)
+    else
+      puts "#{s.id} has no pin_name node, adding it"
+      s.children.prepend(KiCad.parse('(pin_names(hide yes))')&.value)
+    end
+
+    # If there are pin numbers, hide them:
+    pin_number_node = s.property_node('pin_numbers')
+    if pin_number_node
+      hide(pin_number_node)
+    else
+      puts "#{s.id} has no pin_number node, adding it"
+      s.children.prepend(KiCad.parse('(pin_numbers(hide yes))')&.value)
+    end
+  end
+
+File.open('rewrite.kicad_sym', 'w') { |f| f.puts k.emit }
